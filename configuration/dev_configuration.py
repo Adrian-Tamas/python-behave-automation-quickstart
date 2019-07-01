c17a395e30703353841ed4425fec00ff2794a669
Adrian Tamas <adrian.tamas@endava.com>
2019-07-01 08:29:47 +0300

    fix execution

--- configuration/dev_configuration.py
+++ configuration/dev_configuration.py
@@ -6,7 +6,7 @@ class DevConfiguration:
 
     def __init__(self):
         self.configuration = configparser.ConfigParser(allow_no_value=True)
-        self.configuration.read("dev_config.ini")
+        self.configuration.read("dev_configuration.ini")
         self.backend_url = self.configuration[self.ENV_NAME]["backend_url"]
         self.rp_endpoint = self.configuration[self.ENV_NAME]["rp_endpoint"]
         self.rp_project = self.configuration[self.ENV_NAME]["rp_project"]
--- configuration/test_configuration.py
+++ configuration/test_configuration.py
@@ -6,7 +6,7 @@ class TestConfiguration:
 
     def __init__(self):
         self.configuration = configparser.ConfigParser(allow_no_value=True)
-        self.configuration.read("test_config.ini")
+        self.configuration.read("test_configuration.ini")
         self.backend_url = self.configuration[self.ENV_NAME]["backend_url"]
         self.rp_endpoint = self.configuration[self.ENV_NAME]["rp_endpoint"]
         self.rp_project = self.configuration[self.ENV_NAME]["rp_project"]
